9a0199d895293ccc9415d293033f30deaa99d9d9
Ryan Saxe <ryancsaxe+1@gmail.com>
2026-03-04 02:09:12 -0500

    Polish battle inset seams and align side rail zone boundaries

--- web/src/pages/Game.tsx
+++ web/src/pages/Game.tsx
@@ -1191,7 +1191,7 @@ function GameContent() {
             <div className="flex-1 flex min-h-0 game-surface">
               <main className="flex-1 flex flex-col min-h-0 min-w-0">
                 <div className="zone-divider-bg p-[2px] flex-1 min-h-0 flex flex-col">
-                  <div className="surface-inner-emboss flex-1 min-h-0 flex flex-col">
+                  <div className="surface-inner-emboss p-[1px] flex-1 min-h-0 flex flex-col">
                     <BattlePhase
                       gameState={gameState}
                       actions={actions}
